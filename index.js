1cd43268409fd69095532cc42c68fbd7f24b1a24
radoslav <radipirate@gmail.com>
2016-02-21 10:41:45 +0200

    Add negative lookahead to handle missing use case

--- index.js
+++ index.js
@@ -3,38 +3,38 @@
 var through = require('through2');
 
 var plugin = function (options, sync) {
-    return through.obj(function (file, enc, cb) {
-        if (file.isNull()) {
-            return cb(null, file);
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      return cb(null, file);
+    }
+
+    if (file.isStream()) {
+      return cb(new PluginError('gulp-es6-anno-del-sonar', 'Streaming not supported'));
+    }
+
+    var str = file.contents.toString();
+    var regExPatternSingleLine = /@[A-Z][\w]+\(.*\)/g;
+    str = str.replace(regExPatternSingleLine, '');
+
+    var patternMultiLine = /@[A-Z][\w]+\((\s|\S)*?\)(?!\")/g;
+    var multilineMatches = str.match(patternMultiLine);
+    for (var i in multilineMatches) {
+      str = str.replace(multilineMatches[i], function () {
+        var result = "";
+        for (var k = 0; k < multilineMatches[i].match(/\n/g).length; k++) {
+          result += "\n";
         }
+        return result;
+      });
+    }
+    str = str.replace(patternMultiLine, '');
 
-        if (file.isStream()) {
-            return cb(new PluginError('gulp-es6-anno-del-sonar', 'Streaming not supported'));
-        }
-
-        var str = file.contents.toString();
-        var regExPatternSingleLine = /@[A-Z][\w]+\(.*\)/g;
-        str = str.replace(regExPatternSingleLine, '');
-
-        var patternMultiLine = /@[A-Z][\w]+\((\s|\S)*?\)/g;
-        var multilineMatches = str.match(patternMultiLine);
-        for(var i in multilineMatches){
-            str = str.replace(multilineMatches[i],function(){
-                var result ="";
-                for(var k=0;k<multilineMatches[i].match(/\n/g).length;k++){
-                    result+="\n";
-                }
-                return result;
-            });
-        }
-        str = str.replace(patternMultiLine,'');
-
-        var regExPatternSingle = /@[A-Z][\w]+/g;
-        str = str.replace(regExPatternSingle, '');
-        file.contents = new Buffer(str);
-        this.push(file);
-        cb();
-    });
+    var regExPatternSingle = /@[A-Z][\w]+/g;
+    str = str.replace(regExPatternSingle, '');
+    file.contents = new Buffer(str);
+    this.push(file);
+    cb();
+  });
 };
 
 module.exports = plugin;
